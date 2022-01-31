52d8a8283c6e57e2adf7271a990b2f7fbec9bc2d
Sam Daniele <sdaniele@cyber.nj.gov>
2022-01-30 23:05:52 -0500

    update

--- src/Savings_S2022_Daniele_Espinet.java
+++ src/Savings_S2022_Daniele_Espinet.java
@@ -2,4 +2,63 @@ public class Savings_S2022_Daniele_Espinet extends Account_S2022_Daniele_Espinet
 
 {
 
+private double balance;
+
+public Savings_S2022_Daniele_Espinet()
+
+{
+
+}
+
+public double getBalance() 
+
+{
+	
+return balance;
+
+}
+
+
+public void setBalance(double balance) 
+
+{
+	
+this.balance = balance;
+
+}
+
+public void withdraw(double funds)
+
+{
+	
+if (funds >= 0)
+	
+{
+	
+if (funds <= getBalance())
+	
+{
+	
+setBalance(getBalance() - funds);
+
+}
+
+}
+
+}
+
+public void deposit(double funds)
+
+{
+	
+if (funds > 0)
+	
+{
+	
+balance = funds + balance;
+
+}
+
+}
+
 }
