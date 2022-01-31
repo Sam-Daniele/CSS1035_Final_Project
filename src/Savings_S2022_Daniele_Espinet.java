8165f1ebd51308802d15a73cb27d0d9e403f7e37
Sam Daniele <sdaniele@cyber.nj.gov>
2022-01-30 23:13:09 -0500

    update

--- src/Savings_S2022_Daniele_Espinet.java
+++ src/Savings_S2022_Daniele_Espinet.java
@@ -2,6 +2,7 @@ public class Savings_S2022_Daniele_Espinet extends Account_S2022_Daniele_Espinet
 
 {
 
+<<<<<<< HEAD
 private double balance
 
 public Savings_S2022_Daniele_Espinet()
@@ -61,4 +62,6 @@ balance = funds + balance;
 
 }
 
+=======
+>>>>>>> parent of 52d8a82 (update)
 }
