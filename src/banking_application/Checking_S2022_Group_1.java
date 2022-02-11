bcf7a73a25cbb13337b35ad828983172dbc3456c
paolotru <80401594+paolotru@users.noreply.github.com>
2022-02-10 19:12:11 -0500

    Constructor super

--- src/banking_application/Checking_S2022_Group_1.java
+++ src/banking_application/Checking_S2022_Group_1.java
@@ -8,7 +8,9 @@ public class Checking_S2022_Group_1 extends Account_S2022_Group_1 {
 
 	}
 
-	
+	public Checking_S2022_Group_1(double newBalance) {
+		super(newBalance);
+	}
 
 	public void withdraw(double funds) {
 
--- src/banking_application/Savings_S2022_Group_1.java
+++ src/banking_application/Savings_S2022_Group_1.java
@@ -5,5 +5,8 @@ public class Savings_S2022_Group_1 extends Account_S2022_Group_1 {
 	public Savings_S2022_Group_1() {
 
 	}
+	public Savings_S2022_Group_1(double newBalance) {
+		super(newBalance);
+	}
 
 }
