98e2bee90f606d1e7dc3e216ab04fbfd53ec4aed
Sam Daniele <sam.daniele18@stjohns.edu>
2022-02-18 15:27:41 -0500

    bugs/warnings fixed

--- src/banking_application/Overdraft_Exception.java
+++ src/banking_application/Overdraft_Exception.java
@@ -1,10 +1,7 @@
 package banking_application;
 
 public class Overdraft_Exception extends Exception {
-
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 
 	public Overdraft_Exception(double amount) {
