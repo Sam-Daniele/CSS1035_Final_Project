49c116a8ec2c4a6e2e8bbb60d4246a6afa6dfaea
paolotru <80401594+paolotru@users.noreply.github.com>
2022-02-22 11:55:07 -0500

    Input validation

--- src/banking_application/Banking_Application_S2022_Group_1.java
+++ src/banking_application/Banking_Application_S2022_Group_1.java
@@ -14,31 +14,36 @@ public class Banking_Application_S2022_Group_1 {
 		
 		System.out.println("How much would you like to deposit into your checking account?");
 		
-		double depositValue = deposit.nextDouble();
-
-			try{
-				checkingAccount.deposit(depositValue);
-			}
-			catch(Negative_Exception Neg_Exc){
-				System.out.println("You cannot deposit a negative number into your checking account.");
-			}
+		if(deposit.hasNextDouble()) {
+		
+			double depositValue = deposit.nextDouble();
+	
+				try{
+					checkingAccount.deposit(depositValue);
+				}
+				catch(Negative_Exception Neg_Exc){
+					System.out.println("You cannot deposit a negative number into your checking account.");
+				}
+		}
 		
 		Scanner withdraw = new Scanner(System.in);
 		
 		System.out.println("How much would you like to withdraw from your checking account?");
 		
-		double withdrawValue = withdraw.nextDouble();
+		if(withdraw.hasNextDouble()) {
 		
-			try{
-				checkingAccount.withdraw(withdrawValue);
-			}
-			catch(Negative_Exception Neg_Exc){
-				System.out.println("You cannot withdraw a negative number from your checking account.");	
-			}
-			catch(Overdraft_Exception Overd_Exc) {
-				System.out.println("Balance is not enough to withdraw chosen amount.");
-			}
-		
+			double withdrawValue = withdraw.nextDouble();
+			
+				try{
+					checkingAccount.withdraw(withdrawValue);
+				}
+				catch(Negative_Exception Neg_Exc){
+					System.out.println("You cannot withdraw a negative number from your checking account.");	
+				}
+				catch(Overdraft_Exception Overd_Exc) {
+					System.out.println("Balance is not enough to withdraw chosen amount.");
+				}
+		}
 		System.out.println("The balance of your checking account is " + checkingAccount.getBalance() + " dollars.");
 		
 		deposit.close();
